a071c63a24e72ec9a695f1b01efefd433f6861b3
VamsiPerni <mohanperni777@gmail.com>
2025-07-13 12:14:53 +0530

    Simplify CORS configuration by removing specific origin settings

--- app.js
+++ app.js
@@ -10,16 +10,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin:
-      "https://wealth-query-ai-git-main-mohana-vamsi-pernis-projects.vercel.app/api/v1",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  })
-);
-
-app.options("*", cors());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log("-----");
